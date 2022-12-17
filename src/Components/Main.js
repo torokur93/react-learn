a7ef3f14e20a80cd84a3fb2a6bb32e1558eee593
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 22:42:27 +0100

    simplified card props

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,13 +11,7 @@ export default function Main(){
         return(
             <Card
                 key={cardData.id}
-                img={"/Images/" + cardData.coverImg}
-                rating={cardData.stats.rating}
-                reviewCount={cardData.stats.reviewCount}
-                location={cardData.location}
-                title={cardData.title}
-                price={cardData.price}
-                openSpots={cardData.openSpots}
+                data= {cardData}
             /> 
         )
     })
