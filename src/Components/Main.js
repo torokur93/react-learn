59a68304850b9d5a3c95075d6897459c140690f5
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 19:54:09 +0100

    collage styling

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Collage from "./Collage";
 
 export default function Main(){
     return(
         <div className="Main">
+            <Collage/>
             <h1 className="MainTitle">Fun facts:</h1>
             <ul className="MainList">
                 <li>asdas</li>
