0f67ef7816974e7c5153d14159954ef984591799
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 20:29:58 +0100

    card style

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
+import CardImage from "../Images/airbnb-card.webp"
 
 export default function Card(){
     return(
-        <div>
-            
+        <div className="Card">
+            <img className="CardImage" src={CardImage}/>
+            <p className="CardRating">⭐4,99<span className="CardRegion">(156) Egyesült államok</span></p>
+            <h5 className="CardMotto">Tervezd meg a tökéletes New York-i nyaralást</h5>
+            <h4 className="CardPrice">Akár €18 /fő</h4>
         </div>
     )
 }
