7827d639076ffbba6da50bfadb7c8036e39b4619
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 19:17:41 +0100

    project 1 done

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function Main(){
     return(
-        <h1>Main</h1>
+        <div className="Main">
+            <h1 className="MainTitle">Fun facts:</h1>
+            <ul className="MainList">
+                <li>asdas</li>
+                <li>asdasd</li>
+                <li>fdgdfgdf</li>
+                <li>jhgjghj</li>
+                <li>ztrtzjuztu</li>
+            </ul>
+        </div>
     )
 }
