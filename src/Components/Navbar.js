dee8bf9b840424c141ecea2ea3a4ffe64b174ffe
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 19:35:32 +0100

    navbar modification

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import reactLogo from '../Images/logo192.png'
+import reactLogo from '../Images/Airbnb_Logo.png'
 
 export default function Navbar() {
     return(
         <nav className='Navbar'>
             <img className='Logo' src={reactLogo}></img>
-            <h3 className='NavbarTitle'>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
         </nav>
     )
   }
