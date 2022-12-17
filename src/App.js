87aff44e1e90d461ce29cc9fa8427a0941ba1c6e
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 19:38:41 +0100

    added collage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Main from './Components/Main';
+import Collage from './Components/Collage';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Collage/>
       <Main/>
     </div>
     
---
+++ src/Components/Collage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CollageImage from "../Images/collage.png"
+
+export default function Collage(){
+    return(
+        <img src={CollageImage}></img>
+    )
+}
