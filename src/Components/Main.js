a4f3d0f3dadb7b3baaf79b240e0ec192c109a94c
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 21:54:50 +0100

    using props at cards

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Card from "./Card";
 import Collage from "./Collage";
+import cardImage from "../Images/airbnb-card.webp"
 
 export default function Main(){
     return(
         <div className="Main">
             <Collage/>
-            <Card/>
+            <Card
+                img={cardImage}
+                rating="5.0"
+                reviewCount={150}
+                country="USA"
+                title="hát befosol olyan jó"
+                price={9900}
+            />
+            <Card
+                img={cardImage}
+                rating="10.0"
+                reviewCount={99}
+                country="Japan"
+                title="ez a legjobb akkor is"
+                price={100}
+            />
         </div>
     )
 }
