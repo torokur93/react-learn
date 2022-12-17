af94c0d152b4c74bed39033109d67014b43549e0
torokur93 <71731947+torokur93@users.noreply.github.com>
2022-12-17 20:14:27 +0100

    added card component

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,18 +1,12 @@
 import React from "react";
+import Card from "./Card";
 import Collage from "./Collage";
 
 export default function Main(){
     return(
         <div className="Main">
             <Collage/>
-            <h1 className="MainTitle">Fun facts:</h1>
-            <ul className="MainList">
-                <li>asdas</li>
-                <li>asdasd</li>
-                <li>fdgdfgdf</li>
-                <li>jhgjghj</li>
-                <li>ztrtzjuztu</li>
-            </ul>
+            <Card/>
         </div>
     )
 }
